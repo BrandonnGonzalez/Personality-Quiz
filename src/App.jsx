a03d7ef863ad434977d9dbd60af1bb1445c23bcd
Brandon Gonzalez <brandonyt@icloud.com>
2025-10-31 14:27:26 -0400

    Building out Results page and more of Top level App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   const [answers, setAnswers] = useState([]);
   const [userName, setUserName] = useState("");
   const [element, setElement] = useState("");
-  const [artworkImage, setArtworkImage] = useState(null);
+  //artworkImage is a variable created to hold the state for the artwork image of the MET Museum API. Change this variable name as needed.
+  const [artwork, setArtwork] = useState(null);
   // All variable declarations
   const questions = [
     {
@@ -29,8 +30,64 @@ function App() {
     "Yellow 🟡": "Air",
   };
 
+  // All Handler Functions that will handle when changes occur like: click on an option, submitting the username, and  finishing the quiz.
+  function handleAnswer(answer) {
+    setAnswers([...answers, answer]);
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
+  
+  function handleUserFormSubmit(name) {
+    setUserName(name);
+  };
+  
+  function determineElement(answers) {
+    const counts = {};
+    answers.forEach(function(answer) {
+      const element = elements[answer];
+      counts[element] = (counts[element] || 0) + 1;
+    });
+    return Object.keys(counts).reduce(function(a, b) {
+      return counts[a] > counts[b] ? a : b
+    });
+  };
+
+  // API for fetching artwork image from the MET Museum API.
+  useEffect(() => { 
+
+    const fetchArtImage = async () => {
+      try {
+        const response = await fetch(`https://collectionapi.metmuseum.org/public/collection/v1/objects
+`);
+        const data = await response.json();
+        if(!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        setArtworkImage(data.primaryImageSmall);
+      } catch (error) {
+        console.error('Error fetching artwork image:', error);
+      }
+      }
+      fetchArtImage();
+  }, []); // Empty dependency array ensures it runs only once on mount
+  // useEffect to determine the element and fetch the artwork image based on the answers
+  useEffect(
+    function () {
+      if (currentQuestionIndex === questions.length) {
+        const selectedElement = determineElement(answers);
+        setElement(selectedElement);
+        fetchArtwork(keywords[selectedElement]);
+      }
+    },
+    [currentQuestionIndex]
+  );
+
+
   return (
-    <h1>Hello World</h1>
+    <div>
+      <UserContext.Provider value={{ name: userName, setName: handleUserFormSubmit }}>
+
+      </UserContext.Provider>
+    </div>
     
   );
 }
