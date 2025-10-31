7413f34d19befc066d5102dba5c16475aedebe4d
Brandon Gonzalez <brandonyt@icloud.com>
2025-10-31 14:12:54 -0400

    BUilding out App.jsx top-level component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,26 +1,21 @@
-function Question({ question, options, selectedOption, onOptionSelect, onNext }) {
+import React from "react";
+
+export default function Question({ question, options, onAnswer }) {
   return (
-    <div className="question-container">
+    <div>
       <h2>{question}</h2>
-      <div className="options-container">
-        {options.map((option, index) => (
+      {options.map(function (option) {
+        return (
           <button
-            key={index}
-            className={`option ${selectedOption === index ? 'selected' : ''}`}
-            onClick={() => onOptionSelect(index)}
+            key={option}
+            onClick={function () {
+              onAnswer(option);
+            }}
           >
             {option}
           </button>
-        ))}
-      </div>
-      {selectedOption !== null && (
-        <button className="next-button" onClick={onNext}>
-          Next
-        </button>
-      )}
+        );
+      })}
     </div>
   );
 }
-
-export default Question;
-
